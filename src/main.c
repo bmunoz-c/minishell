02cf36cff6dfd2d80635f46c42a0a5984141165a
dhsdev <lua@gnomo.net>
2024-10-01 15:24:01 +0200

    First lines oleole

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int			main(int ac, char **av, char **env)
+{
+	ac = 0;
+	av[0] = "hola";
+	printf("Holaa soy un minishell que va a quedar xulisimo\n");
+	printf("%s\n", env[0]);
+}
