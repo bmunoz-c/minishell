3c683ba3dfb682650aaaefac35021e4ccef223de
bmunoz-c <bmunoz-c@student.42barcelona.com>
2024-10-22 18:19:51 +0200

    commit prueba

--- src/expansor/expansor_utils.c
+++ src/expansor/expansor_utils.c
@@ -6,8 +6,24 @@
 /*   By: bmunoz-c <bmunoz-c@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 17:22:37 by bmunoz-c          #+#    #+#             */
-/*   Updated: 2024/10/22 17:22:39 by bmunoz-c         ###   ########.fr       */
+/*   Updated: 2024/10/22 18:16:03 by bmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <minishell.h>
 
+char	*expand_str(char *str, t_data data)
+{
+	int	i;
+
+	i = 0;
+	if  (str[0] == '$' && str[1] == '\0')
+	{
+		return ('$')
+	}
+}
+
+char	*expand_dq_str(char *str, t_data data)
+{
+
+}
