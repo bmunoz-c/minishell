b0fec009903620b36ee1b90aaa00306f1c9e53b4
bmunoz-c <bmunoz-c@student.42barcelona.com>
2024-12-10 22:32:23 +0100

    updated set_error /  cd building

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,9 +1,12 @@
 #include <minishell.h>
 
-void set_error(t_data *data, const char *msg, int err_code)
+void set_error(t_data *data, const char *msg, int err_code, int cd_print)
 {
     free(data->err_msg);
-    data->err_msg = ft_strdup(msg);
+    if (cd_print && msg)
+        data->err_msg = ft_strjoin("cd: ", msg);
+    else
+        data->err_msg = ft_strdup(msg);
     data->err_code = err_code;
 }
 
@@ -13,29 +16,29 @@ int cd_access(t_data *data, char *path)
 
     // Validar que el puntero path no sea NULL
     if (!path)
-        return (set_error(data, PATH_NULL, 1), 1);
+        return (set_error(data, PATH_NULL, 1, 0), 1);
     // Obtener información del archivo o directorio
     if (lstat(path, &st) == -1)
     {
         ft_putstr_fd("minishell: cd: ", 2);
         perror(path);
-        return (set_error(data, INV_PATH, 126), 1);
+        return (set_error(data, INV_PATH, 126, 0), 1);
     }
     // Comprobar existencia
     if (access(path, F_OK) == -1)
     {
         ft_putstr_fd("minishell: cd: ", 2);
         perror(path);
-        return (set_error(data, NOFILEDIR, 127), 1);
+        return (set_error(data, NOFILEDIR, 127, 0), 1);
     }
     // Comprobar si es un directorio
     if (!S_ISDIR(st.st_mode))
-        return (set_error(data, NOTDIR, 126), 1);
+        return (set_error(data, NOTDIR, 126, 0), 1);
     // Comprobar permisos de ejecución
     if (access(path, X_OK) == -1)
-        return (set_error(data, PERM_DENIED, 126), 1);
+        return (set_error(data, PERM_DENIED, 126, 0), 1);
     // Todo está bien
-    return (set_error(data, NULL, 0), 0);
+    return (set_error(data, NULL, 0, 1), 0);
 }
 
 // Obtener el directorio de trabajo (gwd) actual
