98822a96b13dba6e4dafe42427010d95cb095cac
bmunoz-c <bmunoz-c@student.42barcelona.com>
2024-12-17 14:07:16 +0100

    STARTING export.c

---
+++ src/builtins/export.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmunoz-c <bmunoz-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 14:04:14 by bmunoz-c          #+#    #+#             */
+/*   Updated: 2024/12/17 14:06:56 by bmunoz-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+/* 
+- add variable de entorno.
+- si ya existe, sobreescribe.
+- si no hay argumentos, imprime todas las variables de entorno.
+- si hay argumentos, imprime las variables de entorno que coincidan.
+ */
+
+
+void    run_export(t_data *data)
+{
+    
+}
