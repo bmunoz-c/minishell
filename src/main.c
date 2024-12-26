163ee54e1de5b6003d314eb6835cef27d28d1fc8
dhsdev <lua@gnomo.net>
2024-12-26 18:28:41 +0100

    added heredoc func v1

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lua <lua@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 21:30:48 by ltrevin-          #+#    #+#             */
-/*   Updated: 2024/12/18 16:58:08 by lua              ###   ########.fr       */
+/*   Updated: 2024/12/26 18:25:35 by lua              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,43 @@
 
 int g_sig_exit_status = 0;
 
+void heredoc(t_data data, const char *del, int expand)
+{
+	char *line;
+	int fd;
+	t_token *tk;
+
+	fd = open("/tmp/dancingshell_heredoc", O_WRONLY | O_CREAT , S_IRWXU);
+	if(fd < 0)
+		return ;
+	while (42)
+	{
+		line = readline("> ");
+		if (ft_strncmp(line, del, ft_strlen(del) + 1) == 0)
+			break;
+		if(expand)
+		{
+			tk = new_token(ft_strdup(line), WORD);
+			expansor(&tk, &data);
+			free(line);
+			if(tk)
+				line = ft_strdup(tk->content);
+			free_token(tk);
+		}
+		ft_putendl_fd(line, fd);
+		free(line);
+	}
+	close(fd);
+	fd = open("/tmp/dancingshell_heredoc", O_RDONLY , S_IRWXU);
+	while(1)
+	{
+		char *line = get_next_line(fd);
+		if(!line)
+			break;
+		printf("%s", line);
+	}
+	close(fd);
+}
 
 int	main(int ac, char **av, char **env)
 {
@@ -28,6 +65,7 @@ int	main(int ac, char **av, char **env)
 	}
 	init_data(&data);
 	copy_env(env, &data);
+	heredoc(data, "hola", 1);
 	while (42)
 	{
 		printf(RED "$?: %d\n" RESET, data.err_code);
@@ -36,25 +74,20 @@ int	main(int ac, char **av, char **env)
 		dirty_prompt = readline(PROMPT);
 		if (!dirty_prompt)
 			continue ;
+		add_history(dirty_prompt);
 		data.prompt = ft_strtrim(dirty_prompt, " ");
 		free(dirty_prompt);
 		dirty_prompt = NULL;
 		if (!data.prompt)
 			continue ;
-		// if (!ft_strncmp(data.prompt, "exit", 4))
-		// 	break ;
-		if (!ft_strncmp(data.prompt, "printenv", 8))
-			print_env(data.env);
-		//printf("prompt: %s\n", data.prompt);
 		set_sig_ignore(SIGINT);
 		tokenizer(&data, 0);
 		//print_token_list(data.token_list);
 		expansor(&data.token_list, &data);
 		merge_tokens(&data.token_list);
-		//print_token_list(data.token_list);
+		print_token_list(data.token_list);
 		syntax_error(&data, &data.token_list);		
 		execute(&data);
-		//ft_free_split(array);
 		free_data(&data, 0);
 	}
 	free_data(&data, 1);
