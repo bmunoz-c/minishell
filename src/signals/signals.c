6c476399f12a8dcffbc5eea2e0381040d438a8f3
bmunoz-c <bmunoz-c@student.42barcelona.com>
2025-01-17 17:50:00 +0100

    normineteando

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,40 +6,37 @@
 /*   By: bmunoz-c <bmunoz-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 20:29:28 by bmunoz-c          #+#    #+#             */
-/*   Updated: 2025/01/16 16:24:29 by bmunoz-c         ###   ########.fr       */
+/*   Updated: 2025/01/17 17:49:07 by bmunoz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-/* If mode == 1, recibe SIGINT:
-- Limpia la línea actual y pasa a una nueva línea.
-- Redibuja el prompt para que el usuario pueda continuar escribiendo
-sin interrupción.
-- Actualiza la variable global g_sig para señalar que se ha recibido SIGINT. */
-
+/*
+- mode == 1, recibe SIGINT: (Modo interactivo)
+- mode == 2, recibe SIGINT y SIGQUIT: (Modo no interactivo)
+- mode == 3, recibe SIGINT y SIGQUIT: (Modo heredoc)
+*/
 int	init_signals(int mode)
 {
-	// Modo interactivo.
 	if (mode == 1)
 	{
 		signal(SIGINT, ctrlc_handler);
-		signal(SIGQUIT, SIG_IGN); // Ignorar SIGQUIT en modo interactivo
+		signal(SIGQUIT, SIG_IGN);
 	}
-	// Modo no interactivo.
 	else if (mode == 2)
 	{
 		signal(SIGINT, exit_signal_handler);
 		signal(SIGQUIT, exit_signal_handler);
 	}
-	// HERE_DOC HANDLER
-	/* else if (mode == 3)
+	else if (mode == 3)
 	{
 		signal(SIGINT, heredoc_handler);
 		signal(SIGQUIT, heredoc_handler);
-	} */
+	}
 	return (0);
 }
+
 // Ignorar señales.
 /*signal() devuelve SIG_ERR si ocurre un error al intentar
 configurar el manejador. En ese caso, se llama a exit(1).*/
@@ -49,32 +46,29 @@ void	set_sig_ignore(int signum)
 		exit(1);
 }
 
-// Mantener el control de la línea de comandos tras Ctrl + C.
+/* 
+Mantener el control de la línea de comandos tras Ctrl + C.
+- El cursor pasa a una nueva línea en la terminal.
+- Borrar el contenido de la línea actual.
+- Registrar que se inicia una nueva línea.
+- Redibujar el prompt en la nueva línea limpia.
+- Detecta que el usuario presionó CTRL + C
+
+*/
 void	ctrlc_handler(int sig)
 {
 	(void)sig;
 	{
-		// El cursor pasa a una nueva línea en la terminal.
 		ft_putstr_fd("\n", 1);
-		// Borrar el contenido de la línea actual.
 		rl_replace_line("", 1);
-		// Registrar que se inicia una nueva línea.
 		rl_on_new_line();
-		// Redibujar el prompt en la nueva línea limpia.
 		rl_redisplay();
-		// Detecta que el usuario presionó CTRL + C
 		g_sig_exit_status = 1;
 	}
 }
+
 /*
 Interrumpe el proceso de heredoc:
-- Detecta cuando el usuario presiona CTRL + C (que genera la señal SIGINT).
-- Limpia la línea actual con rl_replace_line("", 1),
-	la entrada del usuario no se queda visible en la terminal.
-
-Restablece el prompt:
-- rl_on_new_line() indica que comienza una nueva línea.
-- rl_redisplay() redibuja el prompt limpio; se interrumpió el heredoc.
 
 Notifica al programa del evento:
 - sig_exit_status = 1 -> la señal SIGINT interrumpió el proceso.
@@ -107,19 +101,14 @@ pero también termina el proceso con el código 130.
 - SIGINT: = 2
 - SIGQUIT: = 3
 */
-
-/* Se utiliza cuando el programa debe finalizar inmediatamente tras recibir SIGINT o SIGQUIT,
-estableciendo códigos de salida (130 o 131) antes de salir. */
 void	exit_signal_handler(int sig)
 {
-	// CTRL + C
 	if (sig == SIGINT)
 	{
 		g_sig_exit_status = 130;
 		printf("\n");
 		exit(130);
 	}
-	// CTRL + \.
 	else if (sig == SIGQUIT)
 	{
 		g_sig_exit_status = 131;
